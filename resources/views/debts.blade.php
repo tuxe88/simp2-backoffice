1ae62d447ec17df70500a85eae5ee1972453758b
Valen <valentin@microblet.com>
2021-08-11 17:26:01 -0300

    added laravel debug bar

--- resources/views/debts.blade.php
+++ resources/views/debts.blade.php
@@ -342,7 +342,7 @@
                             <br>
                             <div class="col-md-12 col-lg-12">
                                 @foreach($response["payment_methods"] as $pm)
-                                    {{var_dump($pm)}}
+                                    {{print_r($pm)}}
                                     <div class="form-group m-0">
                                         <div class="form-group">
                                             <label class="custom-switch">

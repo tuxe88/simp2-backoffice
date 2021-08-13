e44915b3b3321deadc51303fee650751ff1ed3f5
Valen <valentin@microblet.com>
2021-08-13 15:25:45 -0300

    added text

--- resources/views/debts.blade.php
+++ resources/views/debts.blade.php
@@ -682,7 +682,7 @@
                 "unique_reference":unique_ref,
                 "amount":amount,
                 "due_date":due_date,
-                "texts":[[text]]
+                "texts":text
             };
             var json_array = JSON.stringify(arr_value);
             console.log(json_array);
